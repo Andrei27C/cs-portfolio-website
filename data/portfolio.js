2d48cafcb4e18292696631e9e931b9549aaeca8b
Andrei27C <csatlos_andrei27@yahoo.com>
2023-08-13 18:24:58 +0300

    First commit

---
+++ data/portfolio.js
@@ -0,0 +1,86 @@
+const about = {
+  // all the properties are optional - can be left empty or deleted
+  // each element in the description array is a paragraph
+  name: 'John Doe',
+  role: 'Software Engineer',
+  company: 'Acme Labs',
+  description: [
+    'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
+  ],
+  resume: 'https://example.com',
+  social: {
+    linkedin: 'https://linkedin.com',
+    github: 'https://github.com',
+  },
+  greetingEmoji: '👋',
+}
+
+const projects = [
+  // projects can be added and removed
+  // if there are no projects, Projects section won't show up
+  // each element in the description array is a paragraph
+  {
+    thumbnail: 'images/placeholder-image.jpeg',
+    name: 'Project 1',
+    description: [
+      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+    ],
+    stack: ['SASS', 'TypeScript', 'React'],
+    sourceCode: 'https://github.com',
+    livePreview: 'https://github.com',
+  },
+  {
+    thumbnail: 'images/placeholder-image.jpeg',
+    name: 'Project 2',
+    description: [
+      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+    ],
+    stack: ['SASS', 'TypeScript', 'React'],
+    sourceCode: 'https://github.com',
+    livePreview: 'https://github.com',
+  },
+  {
+    thumbnail: 'images/placeholder-image.jpeg',
+    name: 'Project 3',
+    description: [
+      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+    ],
+    stack: ['SASS', 'TypeScript', 'React'],
+    sourceCode: 'https://github.com',
+    livePreview: 'https://github.com',
+  },
+  {
+    thumbnail: 'images/placeholder-image.jpeg',
+    name: 'Project 4',
+    description: [
+      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+    ],
+    stack: ['SASS', 'TypeScript', 'React'],
+    sourceCode: 'https://github.com',
+    livePreview: 'https://github.com',
+  },
+]
+
+const skills = [
+  // skills can be added or removed
+  // if there are no skills, Skills section won't show up
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'TypeScript',
+  'React',
+  'Redux',
+  'SASS',
+  'Material UI',
+  'Git',
+  'CI/CD',
+  'Jest',
+  'Enzyme',
+]
+
+const contact = {
+  // email is optional - if left empty Contact section won't show up
+  email: 'johndoe@mail.com',
+}
+
+export { about, projects, skills, contact }
